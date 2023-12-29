cac39a11bd465278d24930ce53545df0abe16b3f
Saumya <surasaumya17@gmail.com>
2023-12-29 08:01:16 +0530

    ARRAYS

---
+++ arrays/src/main/kotlin/index of an array.kt
@@ -0,0 +1,7 @@
+fun main(args:Array<String>)
+{
+    val names = arrayOf("John","Amit","sam")
+    println("First name is ${names[0]}")
+    println("Second name is ${names[1]}")
+    println("Third elements is ${names[2]}")
+}
